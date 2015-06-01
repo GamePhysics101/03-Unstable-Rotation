65f3ac943739606efd7523d03671ff8fb3fd1c0b
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-06-01 20:15:34 +0100

    GP S03 04 Unstable Rotation In Unity 5.mp4

---
+++ Assets/START Pack/Scripts/InitialKick.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+public class InitialKick : MonoBehaviour {
+
+	private Rigidbody rigidBody;
+	
+	// Use this for initialization
+	void OnEnable () {
+		rigidBody = GetComponent<Rigidbody> ();
+		rigidBody.angularVelocity = new Vector3 (4f, 0, 0);
+	}
+}
---
+++ Assets/START Pack/Scripts/SpinRite.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Rigidbody))]
+public class SpinRite : MonoBehaviour {
+	// __ prefix = order 2 tensor. _ = order 1. no underscore = scalar.
+	private Matrix4x4 __localI; 
+	private Vector3 _globalL;
+
+	private Rigidbody rigidBody;
+	
+	void Start() {
+		// This creates the diagonalised inertia tensor matrix from the Vector3 by the physics engine.
+		// Note the physics engine is using the colliders of the object and it's children, and
+		// the mass of the parent object to calculate an approximate inertia tensor.
+		rigidBody = GetComponent <Rigidbody> ();
+		__localI = Matrix4x4.Scale (rigidBody.inertiaTensor);
+		_globalL = __localI * rigidBody.angularVelocity * 0.1f;
+	}
+
+	void FixedUpdate() {
+		CalculateRotation ();
+	}
+
+	void CalculateRotation ()
+	{
+		// Rotation matrix from world axis, to current object axis.
+		Matrix4x4 __rotationMatrix = Matrix4x4.TRS (Vector3.zero, transform.rotation, Vector3.one);
+
+		// Transform inertia tensor from local to global (L' = R _I R^-1).
+		Matrix4x4 __globalI =  __rotationMatrix * __localI * __rotationMatrix.inverse;
+
+		// Calculate angular velocity by multiplying inverse (L = I w ==> w = I^-1 L)
+		Vector3 _globalW = __globalI.inverse * _globalL;
+	
+		Vector3 _globalRotationAxis = 	_globalW.normalized;
+		float speed = 					_globalW.magnitude;
+
+		float degreesThisFrame = speed * Time.deltaTime * Mathf.Rad2Deg;
+		transform.RotateAround (transform.position, _globalRotationAxis, degreesThisFrame);
+	}
+}
