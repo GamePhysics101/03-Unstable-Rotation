1b592c4c4f7fdd7771039aafcd7af2bd7293ad4f
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-06-08 17:43:45 +0100

    GP S03 05 Introducing Inertia Tensors.mp4

---
+++ Assets/Scripts/ShowStats.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ShowStats : MonoBehaviour {
+
+	private Rigidbody rigidBody;
+	
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (rigidBody.inertiaTensor);
+	}
+}
