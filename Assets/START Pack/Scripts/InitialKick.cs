98b42a247d656fb4ee0b5b8866e87f8c5c906839
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-06-08 20:57:27 +0100

    GP S03 06 Calculating Moment Of Inertia.mp4

--- Assets/START Pack/Scripts/InitialKick.cs
+++ Assets/START Pack/Scripts/InitialKick.cs
@@ -4,11 +4,13 @@ using System.Collections;
 [RequireComponent (typeof (Rigidbody))]
 public class InitialKick : MonoBehaviour {
 
+	public Vector3 initialKick = new Vector3 (4f, 0, 0);
+
 	private Rigidbody rigidBody;
 	
 	// Use this for initialization
 	void OnEnable () {
 		rigidBody = GetComponent<Rigidbody> ();
-		rigidBody.angularVelocity = new Vector3 (4f, 0, 0);
+		rigidBody.angularVelocity = initialKick;
 	}
 }
